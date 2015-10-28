b587aeb314d1ec03cfed40adf263cdf0ac1e50a4
goldrush <goldrush@tk2-260-39818.vs.sakura.ne.jp>
2015-10-28 15:12:32 +0900

    modi

--- dbbackup.sh
+++ dbbackup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 MYSQLDUMP=/usr/bin/mysqldump
 GZIP=/bin/gzip
@@ -8,7 +8,14 @@ if [ -z "$1" ];then
   exit 1
 fi
 
+ping -c 1 $2 > /dev/null
+if [[ $? -ne 0 ]]
+then
+  echo "ping timeout $2" >&2
+  exit 2
+fi
+
 cd $1
 
-$MYSQLDUMP -u gr --password=gr gr --default-character-set=binary | $GZIP > dump.`date +%Y%m%d_%H%M%S`.sql.gz
+$MYSQLDUMP -u gr --password=gr -h $2 gr --default-character-set=binary | $GZIP > dump.`date +%Y%m%d_%H%M%S`.sql.gz
 
--- remotebackup.sh
+++ remotebackup.sh
@@ -1,5 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
-cd /var
-rsync -a --delete goldrush app@dev.applicative.jp:~/backup
+if [[ "$#" < "3" ]]
+then
+  echo "$0 FROM_DIR REMOTE_IP REMOTE_DIR" >&2
+  exit 1
+fi
+
+if [[ ! -d $1 ]]
+then
+  echo "$1 is not dir"
+  exit 1
+fi
+
+ping -c 1 $2 > /dev/null
+if [[ $? -ne 0 ]]
+then
+  echo "ping timeout $2" >&2
+  exit 2
+fi
+
+ssh $2 "ls $3" > /dev/null
+if [[ $? -ne 0 ]]
+then
+  echo "$3 is not remote dir" >&2
+  exit 3
+fi
+
+cd
+rsync -a $1 $2:$3
 
