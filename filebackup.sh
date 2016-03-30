c4b91641c6179450367c32f27449254d3ac59394
goldrush <goldrush@www5239up.sakura.ne.jp>
2016-03-30 15:42:47 +0900

    iii

--- filebackup.sh
+++ filebackup.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 if [ -z "$1" ];then
-  echo "error!! not apply dir path"
+  echo "error!! not apply dir path $1"
   exit 1
+fi
+
+if [ -z "$2" ];then
+  echo "error!! not apply dir path $2"
+  exit 2
 fi
 
 cd $1
 
-tar czf ../backup/files.`date +%Y%m%d_%H%M%S`.tar.gz files
+tar czf $2/files.`date +%Y%m%d_%H%M%S`.tar.gz files
 
